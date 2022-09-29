78ad3f35d8af4ba74daa15d160d3e7e4f9eec987
skyone <1119858711@qq.com>
2022-09-29 01:57:09 +0000

    Start Pre-Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
-import Link from 'next/link'
+
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, {siteTitle} from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  // console.log("Allpostdata",allPostsData)
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`} >
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+
+          ))}
+        </ul>
+
+      </section>
 
       {/* <section className={utilStyles.headingMd}> 
         <p>[Your Self Introduction]</p>
