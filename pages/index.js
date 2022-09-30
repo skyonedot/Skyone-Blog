0606914133c4fc20d86bece08cf953144c41daf5
skyone <1119858711@qq.com>
2022-09-30 02:54:48 +0000

    Done blog

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ export default function Home({allPostsData}) {
 
       <Head>
         <title>{siteTitle}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous"></link>
       </Head>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`} >
