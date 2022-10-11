af8b7aa4c2b9be650fb411a8b5be77156b698a8b
skyone <1119858711@qq.com>
2022-10-11 12:16:37 +0000

    Clean pakcage and chagne article blockquote

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -28,13 +28,13 @@ export default function Post({postData}){
         <Layout>
             <Head>
                 <title>{postData.title}</title> 
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossorigin="anonymous"></link>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous"></link>
                 {/* <link href="/theme/prism-lucario.css" rel="stylesheet"/> */}
                 {/* <link href="/theme/prism-coldark-cold.css" rel="stylesheet"/> */}
                 <link href="/prism.css" rel="stylesheet"/>
                 <script src='/prism.js'></script>
             </Head>
-            <article>
+            <article className={utilStyles.article}>
                 <h1 className={utilStyles.headingX1}> {postData.title} </h1> 
                 <div className={utilStyles.lightText}>
                     <Date dateString={postData.date} />
