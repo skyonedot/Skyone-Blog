bef1b0bbb55a25a44e43db6d5600b5a8b2df6b0d
skyone <1119858711@qq.com>
2022-10-12 15:23:21 +0000

    Eslint Test

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,31 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Script from 'next/script';
 
-import Head from 'next/head'
-import Link from 'next/link'
-import Script from 'next/script'
+import Date from '../components/date';
+import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 
-import Date from '../components/date'
-import utilStyles from '../styles/utils.module.css'
-import Layout, {siteTitle} from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
-
-
-export async function getStaticProps(){
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   // console.log("Allpostdata",allPostsData)
   return (
-    
+
     <Layout home>
-    {/* <Html> */}
-    <div>
-      {/* {console.log('process.env.NEXT_PUBLIC_ANALYTICS_ID',process.env.NEXT_PUBLIC_ANALYTICS_ID)} */}
+      {/* <Html> */}
+      <div>
+        {/* {console.log('process.env.NEXT_PUBLIC_ANALYTICS_ID',process.env.NEXT_PUBLIC_ANALYTICS_ID)} */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-8R6QKXZXYE`}
+          src="https://www.googletagmanager.com/gtag/js?id=G-8R6QKXZXYE"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -39,19 +37,19 @@ export default function Home({allPostsData}) {
             gtag('config', 'G-8R6QKXZXYE');
           `}
         </Script>
-    </div>
+      </div>
 
       <Head>
         <title>{siteTitle}</title>
 
       </Head>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`} >
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
         <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
+          {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}> 
+              <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
 
@@ -65,7 +63,7 @@ export default function Home({allPostsData}) {
 
       </section>
 
-      {/* <section className={utilStyles.headingMd}> 
+      {/* <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
           Hello man
@@ -73,6 +71,5 @@ export default function Home({allPostsData}) {
        </section> */}
     </Layout>
     // </Html>
-  )
+  );
 }
-
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,45 +1,48 @@
-import Date from '../../components/date'
-import Layout from '../../components/layout'
-import { getAllPostIds,getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head';
+import Date from '../../components/date';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticPaths(){
-    const paths = getAllPostIds()
-    return {
-        paths,
-        fallback: false
-    }
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-
-export async function getStaticProps({params}){
-    const postData = await getPostData(params.id);
-    return {
-        props:{
-            postData
-        }
-    }
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
-export default function Post({postData}){
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title> 
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous"></link>
-                {/* <link href="/theme/prism-lucario.css" rel="stylesheet"/> */}
-                {/* <link href="/theme/prism-coldark-cold.css" rel="stylesheet"/> */}
-                <link href="/prism.css" rel="stylesheet"/>
-                <script src='/prism.js'></script>
-            </Head>
-            <article className={utilStyles.article}>
-                <h1 className={utilStyles.headingX1}> {postData.title} </h1> 
-                <div className={utilStyles.lightText}>
-                    <Date dateString={postData.date} />
-                </div>
-                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
-            </article>
-        </Layout>
-    )
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
+        {/* <link href="/theme/prism-lucario.css" rel="stylesheet"/> */}
+        {/* <link href="/theme/prism-coldark-cold.css" rel="stylesheet"/> */}
+        <link href="/prism.css" rel="stylesheet" />
+        <script src="/prism.js" />
+      </Head>
+      <article className={utilStyles.article}>
+        <h1 className={utilStyles.headingX1}>
+          {' '}
+          {postData.title}
+          {' '}
+        </h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
