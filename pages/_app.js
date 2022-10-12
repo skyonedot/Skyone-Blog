6aaa988fae1c1beb9ee998ab260810c659b98797
skyone <1119858711@qq.com>
2022-10-12 16:05:43 +0000

    Next EsLint

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/global.css';
+import '../styles/prism.css';
+// @import 'prysm.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -29,8 +29,8 @@ export default function Post({ postData }) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
         {/* <link href="/theme/prism-lucario.css" rel="stylesheet"/> */}
         {/* <link href="/theme/prism-coldark-cold.css" rel="stylesheet"/> */}
-        <link href="/prism.css" rel="stylesheet" />
-        <script src="/prism.js" />
+        {/* <link href="/prism.css" rel="stylesheet" /> */}
+        <script src="/prism.js" async/>
       </Head>
       <article className={utilStyles.article}>
         <h1 className={utilStyles.headingX1}>
